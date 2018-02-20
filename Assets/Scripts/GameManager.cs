5920db02eb5db46d09024ac24821cb16755d9716
Nuno Barreto <nunobarreto6@gmail.com>
2018-02-20 21:02:14 +0000

    rough populate

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public List<GameObject> PopulatableObjects;
+
+	// Use this for initialization
+	void Start () {
+        populate();
+    }
+
+    void populate() {
+        foreach (GameObject obj in PopulatableObjects)
+        {
+            int randX = (int)Random.Range(-100f, 250f);
+            int randY = (int)Random.Range(-20f, 20f);
+            Instantiate(obj, new Vector3(Random.Range(-100f, 250f), Random.Range(-20, 20), 0), obj.transform.rotation);
+            Instantiate(obj, new Vector3(Random.Range(-100f, 250f), Random.Range(-20, 20), 0), obj.transform.rotation);
+            Instantiate(obj, new Vector3(Random.Range(-100f, 250f), Random.Range(-20, 20), 0), obj.transform.rotation);
+            Instantiate(obj, new Vector3(Random.Range(-100f, 250f), Random.Range(-20, 20), 0), obj.transform.rotation);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
